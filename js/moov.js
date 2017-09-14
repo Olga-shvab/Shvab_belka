2c2f72e005182985fd9649c5d55f47a42aaee4c2
Olga <olgashvab@yandex.ru>
2017-09-14 14:15:55 +0300

    доработка 320p

--- js/moov.js
+++ js/moov.js
@@ -1,3 +1,4 @@
+/* слайд для телефона */
 $(document).ready(function(){
 
 
@@ -17,11 +18,33 @@ $(window).resize(sld);
 });
 
 
+
+/*-----------*/
 $(document).ready(function(){
 
 
 var sld = function() {
-  if ($(window).width() <= 2000) {
+  if ($(window).width() <= 320) {
+
+      	$('.service-photo').slick({
+      		infinite: true,
+      		
+		});
+   
+   } 
+};
+sld();
+$(window).resize(sld);
+
+});
+/*-----------*/
+
+
+$(document).ready(function(){
+
+
+var sld = function() {
+  if ($(window).width() > 320) {
 
       	$('.comments-wrap').slick({
       		slidesToShow: 3,
@@ -37,6 +60,31 @@ $(window).resize(sld);
 
 });
 
+
+
+
+
+
+$(document).ready(function(){
+
+
+var sld = function() {
+  if ($(window).width() <= 320) {
+
+      	$('.comments-wrap').slick({
+      		slidesToShow: 1,
+  			slidesToScroll: 1,
+  			infinite: true
+			
+  			
+      		
+		    });  
+ 	    } 
+	};
+sld();
+$(window).resize(sld);
+
+});
 
 
 /* скрипт прокрутки страницы наверх */
@@ -65,3 +113,86 @@ $(document).ready(function(){
  
 	});
 });
+
+
+
+$(document).ready(function() {								 
+	$('.choose-time').on('click', function() {
+		if ($(".td-mobile-block").css("display") != "block")
+		    $('.td-mobile-block').css('display', 'block')
+			
+		
+		else 
+			$('.td-mobile-block').css('display', 'none')
+	});
+});
+
+$(document).ready(function() {	
+	function accordion()							 
+	 {
+				$(this).toggleClass('price-table__mobile-icon-down');
+	 			 $('.price-table__mobile-icon').not(this).removeClass('price-table__mobile-icon-down');
+	 			}
+	 	$('.price-table__mobile-icon').on('click', accordion);
+		
+	
+});
+
+
+$(document).ready(function() {								 
+	$('.price-table__td--choose1').on('click', function() {
+		if ($(".price-table__td--color").css("display") != "block")
+		    $('.price-table__td--color').css('display', 'block')
+		   
+		
+			
+		else 
+			$('.price-table__td--color').css('display', 'none')
+	});
+});
+
+
+
+
+
+
+$(document).ready(function() {								 
+	$('.choose-place').on('click', function() {
+		if ($(".price-table__td--color-left").css("display") != "block")
+		    $('.price-table__td--color-left').css('display', 'block'),
+	    	$('.choose-time').css('display', 'none')
+		
+		else 
+			$('.price-table__td--color-left').css('display', 'none'),
+		    $('.choose-time').css('display', 'block')
+	});
+});
+
+
+
+
+
+
+$(document).ready(function() {								 
+	$('.icon-dot-3').on('click', function() {
+		 
+			$('.main-nav').css('display', 'block')
+	});
+});
+/*
+$(document).ready(function() {								 
+	$('.price-table__td--color').on('click', function() {
+		 
+			$('.price-table__td--color').css('display', 'none')
+	});
+});
+
+
+$(document).ready(function() {								 
+	$('.price-table__td--choose2').on('click', function() {
+		 
+			$('.price-table__td--choose2').css('display', 'none')
+	});
+});
+
+*/
