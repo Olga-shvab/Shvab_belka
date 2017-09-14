5d89c1689e6fb62decd17c8610f0b62bd3725fb3
Olga <olgashvab@yandex.ru>
2017-09-14 20:42:36 +0300

    доработка mobile не до конца

--- js/moov.js
+++ js/moov.js
@@ -19,6 +19,29 @@ $(window).resize(sld);
 
 
 
+
+$(document).ready(function(){
+
+
+var sld = function() {
+  if ($(window).width() > 320 & ($(window).width() <= 768)    ) {
+
+      	$('.track-container').slick({
+      		slidesToShow: 3,
+  			slidesToScroll: 1,
+			autoplay: true,
+  			autoplaySpeed: 3000
+      		
+		});
+   
+   } 
+};
+sld();
+$(window).resize(sld);
+
+});
+
+
 /*-----------*/
 $(document).ready(function(){
 
@@ -127,6 +150,9 @@ $(document).ready(function() {
 	});
 });
 
+
+
+
 $(document).ready(function() {	
 	function accordion()							 
 	 {
@@ -142,12 +168,19 @@ $(document).ready(function() {
 $(document).ready(function() {								 
 	$('.price-table__td--choose1').on('click', function() {
 		if ($(".price-table__td--color").css("display") != "block")
-		    $('.price-table__td--color').css('display', 'block')
+		    $('.price-table__td--color').css('display', 'block'),
+	    	$('.choose-time').css('display', 'none'),
+
+	    	$('.choose-place').css('display', 'none'),
+	    	$('.tr-mobile-block').css('display', 'none')
 		   
 		
 			
 		else 
-			$('.price-table__td--color').css('display', 'none')
+			$('.price-table__td--color').css('display', 'none'),
+			 $('.choose-time').css('display', 'block'),
+			  $('.choose-place').css('display', 'block'),
+			  $('.tr-mobile-block').css('display', 'block')
 	});
 });
 
@@ -160,11 +193,14 @@ $(document).ready(function() {
 	$('.choose-place').on('click', function() {
 		if ($(".price-table__td--color-left").css("display") != "block")
 		    $('.price-table__td--color-left').css('display', 'block'),
-	    	$('.choose-time').css('display', 'none')
+	    	$('.choose-time').css('display', 'none'),
+	    	$('.tr3-mobile-block').css('display', 'block')
 		
 		else 
 			$('.price-table__td--color-left').css('display', 'none'),
-		    $('.choose-time').css('display', 'block')
+		    $('.choose-time').css('display', 'block'),
+		    $('.tr3-mobile-block').css('display', 'none')
+
 	});
 });
 
